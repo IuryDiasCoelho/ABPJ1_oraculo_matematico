cb69d79d18ed5e22548618ce5aad2f47aeccb194
IuryDiasCoelho <iurydiascoelho31@hotmail.com>
2026-02-03 21:05:47 -0300

    finalização do codigo e logica e atualização do readme.

--- README.md
+++ README.md
@@ -2,41 +2,40 @@
 
 > Um projeto academico para aprofundamento em python
 
----
-
-## 📋 Índice
-* [Sobre](#-sobre)
-* [Funcionalidades](#-funcionalidades)
-* [Tecnologias](#-tecnologias)
-* [Como Rodar](#-como-rodar)
-* [Estrutura de Pastas](#-estrutura-de-pastas)
-* [Licença](#-licença)
-
----
-
 ## 💻 Sobre
 Esse projeto tem como finalidade calcular o valor total de 
-energia de uma residencia e informar uma economia ou prejuizo.
+energia de uma residencia e informar uma economia ou prejuizo entre os meses.
 
 ## ✨ Fluxograma
 ![fluxograma.png](fluxograma.png)
 
 ## 🛠 Tecnologias
 As principais ferramentas usadas no projeto:
-* **Linguagem:** [Python / JavaScript / etc]
-* **Frameworks/Bibliotecas:** [Pandas, Flask, React]
-* **Banco de Dados:** [SQLite, PostgreSQL]
-* **Outros:** [Docker, Git LFS]
+* **Linguagem:** [Python]
 
 ## 🚀 Como Rodar
 Siga os passos abaixo para ter uma cópia do projeto rodando na sua 
 máquina:
 
 ### Pré-requisitos
-* [Ex: Python 3.10 ou superior instalado]
-* [Ex: Git instalado]
+* Python 3.10 ou superior instalado
+* Git instalado
 
 ### Instalação
 1. Clone o repositório:
    ```bash
    git clone [https://github.com/usuario/projeto.git](https://github.com/usuario/projeto.git)
+
+2. Acesse a pasta do projeto:
+
+   ```bash
+    cd nome-do-seu-repositorio
+   
+3. Execute o programa:
+
+   ```bash
+    python main.py
+
+## 👩‍💻👨‍💻 Autores
+Informe:
+- Iury Dias Coelho – Desenvolvimento Web e Mobile – Python basico.
---
+++ main.py
@@ -0,0 +1,33 @@
+# entrada de dados do usuario para o mes atual
+mes_atual=input("Digite quantos kilovats foi consumido (apenas os valores. EX:350,2): ")
+
+# tratando erro de usar virgula como separador de casas decimais
+mes_atual_limpo = float(mes_atual.replace(',', '.'))
+preco_kw=(input("Digite se voçe souber o preço atual do kilowatt (apenas os valores. EX: 0.89 ): "))
+preco_kw_limpo=float(preco_kw.replace(',', '.'))
+
+# calculando o valor total do mes atual
+cauculo_mes_atual=mes_atual_limpo*preco_kw_limpo
+print(f"O seu consumo foi de : {mes_atual_limpo} kilowats")
+print(f"O seu preço atual do kilowats foi de : R${preco_kw_limpo}")
+print(f"O seu valor total foi da sua fatura atual foi : R${cauculo_mes_atual}")
+
+print(f"Faremos um comparativo com o valor do mes anterior para saber a economia ou prejuizo entre os meses")
+
+# entrada de dados do usuario para o mes anterior e tratando erro de usar virgula como separador de casas decimais
+mes_anterior=input("Digite quantos kilovats foi consumido no mes anterior (apenas os valores. EX:350,2): ")
+mes_anterior_limpo = float(mes_anterior.replace(',', '.'))
+
+# calculando o valor total do mes anterior
+cauculo_mes_anterior=mes_anterior_limpo*preco_kw_limpo
+
+print(f"O seu valor total foi da sua fatura anterior foi : R${cauculo_mes_atual}")
+
+# cauculando porcentagem entre os valores
+percentagem=(cauculo_mes_atual-cauculo_mes_anterior)/cauculo_mes_anterior*100
+perentagem_final=abs(percentagem)
+
+if percentagem<0:
+    print(f"O seu percentual de prejuizo foi de : {perentagem_final}%")
+else:
+    print(f"O seu percentual de economia foi de : {perentagem_final}%")
